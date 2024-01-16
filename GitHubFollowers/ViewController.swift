22c49e19eff0e6c52e70a48777e4f2bb222f5ca4
altankorayy <altankoraybenli@gmail.com>
2024-01-16 21:26:18 +0300

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Altan on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
